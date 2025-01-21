65c221014a04a9a98783de8e06f2f779a1d62e6b
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-01-22 01:22:49 +0900

    Feat: UI 모듈 생성

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.kolown.ui"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    implementation(libs.bundles.android.compose)
+
+    implementation(projects.core.designsystem)
+}
---
+++ core/ui/src/main/java/kr/co/ui/MyClass.kt
@@ -0,0 +1,4 @@
+package kr.co.ui
+
+class MyClass {
+}
