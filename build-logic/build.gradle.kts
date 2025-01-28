830140fb35c5b478cfcbebbdb51a646cd368b640
Inwoo <lys91892@gmail.com>
2025-01-28 18:18:41 +0900

    Feat: build.gradle.kts, settings.gradle.kts 설정

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,4 +1,10 @@
 plugins {
-
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
 }
 
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
+}
