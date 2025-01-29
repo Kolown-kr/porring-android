d1a5cb44dbba53d46e2dfd77596fce0f51e7b529
Inwoo <lys91892@gmail.com>
2025-01-29 16:06:38 +0900

    Feat: compose convention 설정

---
+++ build-logic/src/main/java/com/kolown/porring/ComposeAndroid.kt
@@ -0,0 +1,39 @@
+package com.kolown.porring
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+
+internal fun Project.configureComposeAndroid() {
+    with(plugins) {
+        apply("org.jetbrains.kotlin.plugin.compose")
+    }
+
+    val libs = extensions.libs
+    androidExtension.apply {
+        dependencies {
+            val bom = libs.findLibrary("androidx-compose-bom").get()
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
+
+            add("implementation", libs.findLibrary("androidx.compose.material3").get())
+            add("implementation", libs.findLibrary("androidx.compose.ui").get())
+            add("implementation", libs.findLibrary("androidx.compose.ui.tooling.preview").get())
+            add("implementation", libs.findLibrary("androidx.activity.compose").get())
+
+            add("implementation", libs.findLibrary("androidx.navigation.compose").get())
+            add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+
+            add("androidTestImplementation", libs.findLibrary("androidx.ui.test.junit4").get())
+
+            add("debugImplementation", libs.findLibrary("androidx.compose.ui.tooling").get())
+            add("debugImplementation", libs.findLibrary("androidx.compose.ui.test.manifest").get())
+        }
+    }
+
+    extensions.getByType<ComposeCompilerGradlePluginExtension>().apply {
+        enableStrongSkippingMode.set(true)
+        includeSourceInformation.set(true)
+    }
+}
