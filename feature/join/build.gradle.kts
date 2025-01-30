8953dbf2f6b950262f51af015e58a5772f452438
Inwoo <lys91892@gmail.com>
2025-01-31 00:20:56 +0900

    Feat: feature.join 모듈 build-logic 적용

--- feature/join/build.gradle.kts
+++ feature/join/build.gradle.kts
@@ -1,59 +1,13 @@
+import com.kolown.porring.setNamespace
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
+    id("porring.android.feature")
 }
 
 android {
-    namespace = "com.kolown.join"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    setNamespace("feature.join")
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    //hilt
-    implementation(libs.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
-    ksp(libs.hilt.android.compiler)
-
     implementation(libs.google.firebase.auth)
-
-    implementation(libs.bundles.android.compose)
-
-    implementation(projects.core.data)
-    implementation(projects.core.navigation)
-    implementation(projects.core.designsystem)
-    implementation(projects.core.common)
 }
