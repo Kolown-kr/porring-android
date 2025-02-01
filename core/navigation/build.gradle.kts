d53b107de0887f718e87067531421bb4680ea232
Inwoo <lys91892@gmail.com>
2025-02-01 23:55:02 +0900

    Feat: model, navigation kotlin module로 변경

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,11 +1,7 @@
-import com.kolown.porring.setNamespace
-
 plugins {
+    alias(libs.plugins.porring.kotlin.library)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.porring.android.library)
 }
-
-setNamespace("core.navigation")
 
 dependencies {
     implementation(libs.kotlinx.serialization.json)
