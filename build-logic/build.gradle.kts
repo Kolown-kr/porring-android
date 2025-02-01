137c0af8dd6acdb06942cdec18f3287cb375bcb2
Inwoo <lys91892@gmail.com>
2025-02-01 21:36:43 +0900

    Fix: build-logic build.gradle 보일러플레이트 코드 삭제

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,12 +8,3 @@ dependencies {
     implementation(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.compiler.gradle.plugin)
 }
-
-gradlePlugin {
-    plugins {
-        register("androidHilt") {
-            id = "droidknights.android.hilt"
-            implementationClass = "com.kolown.porring.HiltAndroidPlugin"
-        }
-    }
-}
