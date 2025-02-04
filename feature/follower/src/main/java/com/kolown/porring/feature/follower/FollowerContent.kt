183ce2ef91002ae56b97fe1b17dba338fa98c880
Inwoo <lys91892@gmail.com>
2025-02-05 00:13:39 +0900

    Feat: Follow 화면 ui 수정

---
+++ feature/follower/src/main/java/com/kolown/porring/feature/follower/FollowerContent.kt
@@ -0,0 +1,107 @@
+package com.kolown.porring.feature.follower
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kolown.porring.core.common.component.CoilImage
+import com.kolown.porring.core.designsystem.ui.theme.Error
+import com.kolown.porring.core.designsystem.ui.theme.Primary
+
+@Composable
+internal fun FollowContent(
+    followerName: String = "name",
+    followAlbums: List<String> = listOf(),
+    navigateToTheir: () -> Unit = {},
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null
+            ) {
+                navigateToTheir()
+            }
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = followerName,
+                color = Primary,
+                style = MaterialTheme.typography.titleLarge
+            )
+            TextButton (
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = stringResource(R.string.string_edit),
+                    color = Error,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(
+                space = 8.dp
+            )
+        ) {
+            items(followAlbums) { imageUrl ->
+                Card(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                ) {
+                    CoilImage(
+                        imageUrl = imageUrl,
+                        onClickEnabled = false
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewFollowContent() {
+    FollowContent()
+}
