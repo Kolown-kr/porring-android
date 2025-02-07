46d6d9088bc736d7b198f6ab5009a48e6a99406b
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-02-07 11:37:57 +0900

    Refactor 검색 이미지 공유 뷰모델로 분리

--- feature/search/src/main/java/com/kolown/porring/feature/search/model/SearchUiSideEffect.kt
+++ feature/search/src/main/java/com/kolown/porring/feature/search/model/SearchUiSideEffect.kt
@@ -1,7 +1,9 @@
 package com.kolown.porring.feature.search.model
 
+import com.kolown.porring.core.model.Tag
 import com.kolown.porring.core.ui.base.UiSideEffect
 
 internal sealed interface SearchUiSideEffect : UiSideEffect {
+    data class FetchPostsByTag(val tag: Tag) : SearchUiSideEffect
     data object NavigateToDetail : SearchUiSideEffect
 }
