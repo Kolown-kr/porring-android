a52f3815946a4a4feeb0b22a063ff6bf17024a21
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-02-10 07:19:57 +0900

    Chore: resource 위치 변경

--- feature/main/src/main/java/com/kolown/porring/feature/main/component/NotAvailableVersionScreen.kt
+++ feature/main/src/main/java/com/kolown/porring/feature/main/component/NotAvailableVersionScreen.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-import com.kolown.porring.core.common.R
+import com.kolown.porring.core.ui.R
 import com.kolown.porring.core.designsystem.ui.theme.Primary
 
 @Composable
