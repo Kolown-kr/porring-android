e26a48a394e95811aed7d8faec898d62206e72c2
Inwoo <lys91892@gmail.com>
2025-02-10 23:57:54 +0900

    Feat: 미사용 코드 삭제

--- feature/my/src/main/java/com/kolown/porring/feature/my/navigation/MyNavigation.kt
+++ feature/my/src/main/java/com/kolown/porring/feature/my/navigation/MyNavigation.kt
@@ -1,14 +1,11 @@
 package com.kolown.porring.feature.my.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.kolown.porring.feature.my.DetailMyRoute
 import com.kolown.porring.feature.my.MyRoute
 import com.kolown.porring.core.navigation.MainMenuRoute
 import com.kolown.porring.core.navigation.Route
@@ -22,39 +19,18 @@ fun NavController.navigateMyDetail(navOptions: NavOptions) {
 }
 
 fun NavGraphBuilder.myNavGraph(
-    isLoggedIn: Boolean = false,
+    padding: PaddingValues,
     navigateToLogin: () -> Unit,
     navigateToSetting: () -> Unit,
-    navigateToDetailMy: () -> Unit,
-    padding: PaddingValues,
-    popBackStack: () -> Unit,
-    getBackStackEntry: () -> NavBackStackEntry
+    navigateToDetail: () -> Unit,
 ) {
-    composable<MainMenuRoute.My> { backStackEntry ->
-        val parentEntry = remember(backStackEntry) {
-            getBackStackEntry()
-        }
+    composable<MainMenuRoute.My> {
         MyRoute(
-            isLoggedIn = isLoggedIn,
             navigateToLogin = navigateToLogin,
             navigateToSetting = navigateToSetting,
-            navigateToDetailMy = navigateToDetailMy,
+            navigateToDetail = navigateToDetail,
             padding = padding,
-            viewModel = hiltViewModel(parentEntry)
-        )
-    }
-
-    composable<Route.DetailMy> { backStackEntry ->
-        val parentEntry = remember(backStackEntry) {
-            getBackStackEntry()
-        }
-        DetailMyRoute(
-            padding = padding,
-            popBackStack = popBackStack,
-            onShowLoginSnackBar = {
-                navigateToLogin()
-            },
-            viewModel = hiltViewModel(parentEntry)
+            viewModel = hiltViewModel()
         )
     }
 }
