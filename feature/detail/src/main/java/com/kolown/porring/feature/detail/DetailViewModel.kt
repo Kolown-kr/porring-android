407802cf35527f9465f4b4dba07f362f2730f5d6
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-02-10 19:05:23 +0900

    Refactor: 디테일 화면 UI

--- feature/detail/src/main/java/com/kolown/porring/feature/detail/DetailViewModel.kt
+++ feature/detail/src/main/java/com/kolown/porring/feature/detail/DetailViewModel.kt
@@ -1,28 +1,22 @@
 package com.kolown.porring.feature.detail
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.paging.map
 import com.kolown.porring.core.data.repository.AuthRepository
 import com.kolown.porring.core.data.repository.FollowRepository
 import com.kolown.porring.core.data.repository.PostRepository
 import com.kolown.porring.core.model.PostContentModel
 import com.kolown.porring.core.model.Reactions
 import com.kolown.porring.core.navigation.MainMenuRoute
+import com.kolown.porring.core.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,79 +24,34 @@ internal class DetailViewModel @Inject constructor(
     authRepository: AuthRepository,
     private val postRepository: PostRepository,
     private val followRepository: FollowRepository,
-) : ViewModel() {
+) : BaseViewModel<DetailViewModel.State>(State()) {
 
     private val _posts = MutableStateFlow<PagingData<PostContentModel>>(PagingData.empty())
     val posts = _posts.cachedIn(viewModelScope)
 
-    private val _followEvent = MutableSharedFlow<PostContentModel>()
-    val followEvent = _followEvent.asSharedFlow()
-
-    private val loggedInChannel = Channel<Unit>(Channel.UNLIMITED)
-    val loggedInEvent = loggedInChannel.receiveAsFlow()
-
-    private val isLoggedIn = authRepository.checkUserLoggedIn()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Eagerly,
-            initialValue = false
-        )
+    val isLoggedIn = authRepository.checkUserLoggedIn()
 
     fun init(
         type: MainMenuRoute.Detail.Type,
+        order: Int,
     ) {
         postRepository.getRandomDetailPostList()
             .onEach(_posts::emit)
             .launchIn(viewModelScope)
+
+        callbackFlow<String> { awaitClose {  } }.buffer()
     }
 
-    private fun checkedLogIn(): Boolean {
-        if (isLoggedIn.value.not()) {
-            loggedInChannel.trySend(Unit)
-            return false
-        }
-        return true
+    fun onModeChange(isReelsMode: Boolean) = updateState {
+        copy(isReelsMode = isReelsMode)
     }
 
-    fun onReactionClick(postId: String, reaction: Reactions) = viewModelScope.launch {
-        if (checkedLogIn().not()) return@launch
-        postRepository.reactPost(postId, reaction)
-        _posts.update {
-            _posts.value.replaceIf(
-                predicate = { it.postId == postId },
-                replacement = { it.copy(myReaction = reaction) }
-            )
-        }
-    }
-
-    fun onFollowClick(post: PostContentModel) = viewModelScope.launch {
-        if (checkedLogIn().not()) return@launch
-        if (post.isFollower) {
-            followRepository.unFollowUser(post.authorId)
-            _posts.update {
-                _posts.value.replaceIf(
-                    predicate = { it.authorId == post.authorId },
-                    replacement = { it.copy(isFollower = false) }
-                )
-            }
-        } else {
-            _followEvent.emit(post)
-        }
-    }
-
-    fun registerFollow(authorId: String, name: String) = viewModelScope.launch {
-        if (checkedLogIn().not()) return@launch
-        followRepository.followUser(authorId, name)
-        _posts.update {
-            _posts.value.replaceIf(
-                predicate = { it.authorId == authorId },
-                replacement = { it.copy(isFollower = true) }
-            )
-        }
-    }
-
-    private inline fun <T : Any> PagingData<T>.replaceIf(
-        crossinline predicate: (T) -> Boolean,
-        crossinline replacement: (T) -> T,
-    ): PagingData<T> = map { if (predicate(it)) replacement(it) else it }
+    data class State(
+        val isReelsMode: Boolean = true,
+    )
 }
+
+data class ReactionState(
+    val prev: Reactions? = null,
+    val current: Reactions = Reactions.LOVE,
+)
