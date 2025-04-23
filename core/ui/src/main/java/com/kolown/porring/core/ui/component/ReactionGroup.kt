dd44ea7a4b30c47937a8c09783e758d1ed3d86df
Inwoo <lys91892@gmail.com>
2025-04-24 00:10:49 +0900

    Refactor: ReactionIcons shadow 적용

--- core/ui/src/main/java/com/kolown/porring/core/ui/component/ReactionGroup.kt
+++ core/ui/src/main/java/com/kolown/porring/core/ui/component/ReactionGroup.kt
@@ -1,5 +1,6 @@
 package com.kolown.porring.core.ui.component
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -10,12 +11,21 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import coil3.compose.AsyncImage
 import com.kolown.porring.core.designsystem.ui.theme.Primary
 import com.kolown.porring.core.model.Reactions
+import com.kolown.porring.core.ui.ext.shadow
 
 @Composable
 fun ReactionGroup(
@@ -60,12 +70,18 @@ private fun ReactionIcons(
         modifier = Modifier
             .size(24.dp)
             .zIndex(-index.toFloat())
+            .shadow(
+                color = Color.Black.copy(0.25f),
+                shape = CircleShape,
+                offsetX = 2.dp,
+                offsetY = 1.dp,
+                blur = 4.dp
+            )
             .background(
                 color = color,
                 shape = CircleShape
             )
             .padding(4.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
         AsyncImage(
             model = reaction.toImage(),
@@ -73,5 +89,4 @@ private fun ReactionIcons(
             modifier = Modifier.size(16.dp)
         )
     }
-
 }
