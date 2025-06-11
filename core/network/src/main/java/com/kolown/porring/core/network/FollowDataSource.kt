9088d8f77b0045c1f6ac19dc016f1f3adf7bce0b
bong-pal <etgt777@naver.com>
2025-06-11 21:50:11 +0900

    Chore: 사용하지 않는 follow data source로직 제거

--- core/network/src/main/java/com/kolown/porring/core/network/FollowDataSource.kt
+++ core/network/src/main/java/com/kolown/porring/core/network/FollowDataSource.kt
@@ -3,19 +3,14 @@ package com.kolown.porring.core.network
 import android.util.Log
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import com.kolown.porring.core.model.Follow
-import com.kolown.porring.core.network.model.FollowerDto
-import com.kolown.porring.core.network.model.toFollowerModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
-import java.io.IOException
 import javax.inject.Inject
 
 interface FollowDataSource {
-    suspend fun getIsFollower(userId: String, followerId: String): Result<Boolean>
     suspend fun uploadFollow(
         userId: String,
         follow: Follow
@@ -23,49 +18,12 @@ interface FollowDataSource {
 
     suspend fun fetchFollows(userId: String): List<Follow>
     suspend fun removeFollow(userId: String, followerId: String): Flow<Boolean>
-    suspend fun getFollowerList(
-        userId: String,
-        key: String?,
-        perPage: Long
-    ): Result<List<Follow>>
 }
 
 class FollowDataSourceImpl @Inject constructor(
     firestore: FirebaseFirestore
 ) : FollowDataSource {
-    private val followCollection = firestore.collection("follow")
     private val userCollection = firestore.collection("user")
-
-    override suspend fun getIsFollower(userId: String, followerId: String): Result<Boolean> {
-        return runCatching {
-            val result = followCollection.contains(userId, followerId).getOrElse {
-                throw IOException("팔로우 확인 에러")
-            }
-
-            result.isEmpty.not()
-        }.onFailure {
-            Log.e("GetFollower", "getIsFollower: $it")
-        }
-    }
-
-    override suspend fun getFollowerList(
-        userId: String,
-        key: String?,
-        perPage: Long
-    ): Result<List<Follow>> {
-        return kotlin.runCatching {
-            val followerIds = followCollection
-                .whereEqualTo("userId", userId)
-                .orderBy("followerId", Query.Direction.DESCENDING)
-                .let { if (key != null) it.startAfter(key) else it }
-                .limit(perPage)
-                .get()
-                .await()
-                .map { it.toObject(FollowerDto::class.java).toFollowerModel() }
-
-            followerIds
-        }
-    }
 
     override suspend fun uploadFollow(userId: String, follow: Follow): Flow<Boolean> = flow {
         userCollection
