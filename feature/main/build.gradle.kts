2688d290ff4855607f02045c09d565d40194c15f
bong-pal <etgt777@naver.com>
2025-06-12 22:12:39 +0900

    File: 데이터 이전 관련 로직 제거
    - reacted post이전을 위해 사용했던 로직들 제거

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -22,6 +22,4 @@ dependencies {
     implementation(projects.feature.their)
     implementation(projects.feature.upload)
     implementation(projects.feature.imageEdit)
-    implementation(libs.firebase.common)
-    implementation(libs.firebase.firestore.ktx)
 }
