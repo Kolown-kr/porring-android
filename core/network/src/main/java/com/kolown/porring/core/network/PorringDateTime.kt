5658a85667b287af30707edebe4b63aaacdaff0d
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-07-09 15:54:00 +0900

    Refactor: PorringDateTime 수정 #PORING-113

--- core/network/src/main/java/com/kolown/porring/core/network/PorringDateTime.kt
+++ core/network/src/main/java/com/kolown/porring/core/network/PorringDateTime.kt
@@ -1,53 +1,62 @@
 package com.kolown.porring.core.network
 
 import android.icu.util.Calendar
+import android.icu.util.TimeZone
 import android.os.Build
-import java.time.LocalDateTime
+import androidx.annotation.RequiresApi
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-class PorringDateTime {
-    companion object {
-        fun getNowDateTimeString(): String {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val current = LocalDateTime.now()
-                val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-                current.format(formatter)
-            } else {
-                // API 26 이하 대응
-                val calendar = Calendar.getInstance()
-                val year = calendar.get(Calendar.YEAR)
-                val month = calendar.get(Calendar.MONTH) + 1 // Month is zero-based
-                val day = calendar.get(Calendar.DAY_OF_MONTH)
-                val hour = calendar.get(Calendar.HOUR_OF_DAY)
-                val minute = calendar.get(Calendar.MINUTE)
-                val second = calendar.get(Calendar.SECOND)
-                String.format(
-                    Locale.US,
-                    "%04d%02d%02d%02d%02d%02d",
-                    year, month, day, hour, minute, second
-                )
-            }
+object PorringDateTime {
+
+    fun getNowDateTimeString(): String = formatCurrentTime(TimeZone.getDefault())
+
+    fun getNowDateTimeUTCString(): String = formatCurrentTime(TimeZone.getTimeZone(UTC))
+
+    private fun formatCurrentTime(
+        timeZone: TimeZone,
+    ) = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        formatTimeApi26(timeZone)
+    } else {
+        formatTimeApi19(timeZone)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatTimeApi26(
+        timeZone: TimeZone
+    ): String = if (timeZone == TimeZone.getTimeZone(UTC)) {
+        ZonedDateTime.now(ZoneOffset.UTC).toString()
+    } else {
+        val formatter = DateTimeFormatter.ofPattern(DEFAULT_PATTERN)
+        ZonedDateTime.now().format(formatter)
+    }
+
+    private fun formatTimeApi19(
+        timeZone: TimeZone,
+    ): String {
+        val calendar = Calendar.getInstance(timeZone)
+
+        val pattern: String = when (timeZone.id) {
+            UTC -> UTC_PATTERN26
+            else -> DEFAULT_PATTERN26
         }
 
-        fun getNowDateTimeUTCString(): String {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                LocalDateTime.now().toString()
-            } else {
-                // API 26 이하 대응
-                val calendar = Calendar.getInstance()
-                val year = calendar.get(Calendar.YEAR)
-                val month = calendar.get(Calendar.MONTH) + 1 // Month is zero-based
-                val day = calendar.get(Calendar.DAY_OF_MONTH)
-                val hour = calendar.get(Calendar.HOUR_OF_DAY)
-                val minute = calendar.get(Calendar.MINUTE)
-                val second = calendar.get(Calendar.SECOND)
-                String.format(
-                    Locale.US,
-                    "%04d-%02d-%02dT%02d:%02d:%02d",
-                    year, month, day, hour, minute, second
-                )
-            }
-        }
+        return String.format(
+            Locale.US,
+            pattern,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH),
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            calendar.get(Calendar.SECOND)
+        )
     }
+
+    private const val UTC = "UTC"
+    private const val DEFAULT_PATTERN = "yyyyMMddHHmmss"
+    private const val DEFAULT_PATTERN26 = "%04d%02d%02d%02d%02d%02d"
+    private const val UTC_PATTERN26 = "%04d-%02d-%02dT%02d:%02d:%02d"
 }
