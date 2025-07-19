4a3e4266eb160da5f28b3657814986446802be93
bong-pal <etgt777@naver.com>
2025-07-19 23:13:19 +0900

    Refactor: reaction dialog 변경
    - IconButton 내부로직 변경
    - reaction dialog -> reaction selector로 이름 변경

--- feature/detail/src/main/java/com/kolown/porring/feature/detail/DetailScreen.kt
+++ feature/detail/src/main/java/com/kolown/porring/feature/detail/DetailScreen.kt
@@ -57,8 +57,8 @@ import com.kolown.porring.core.navigation.Route
 import com.kolown.porring.core.ui.component.BetaPorringAlertDialog
 import com.kolown.porring.core.ui.component.CoilImage
 import com.kolown.porring.core.ui.component.FollowDialog
-import com.kolown.porring.core.ui.component.ReactionDialog
 import com.kolown.porring.core.ui.component.ReactionGroup
+import com.kolown.porring.core.ui.component.ReactionSelector
 import com.kolown.porring.core.ui.component.toImage
 import com.kolown.porring.core.ui.compositionlocal.LocalSnackBarBridge
 import com.kolown.porring.core.ui.model.PostUiModel
@@ -338,7 +338,7 @@ private fun EventRow(
             shape = CircleShape,
             onDismissRequest = { isExpand = false }
         ) {
-            ReactionDialog(
+            ReactionSelector(
                 activatedReaction = activatedReaction,
                 selectedReaction = onReactionClick,
                 onDismiss = { isExpand = false }
