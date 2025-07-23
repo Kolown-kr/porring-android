bc6086b49e9bf19709f2d2513c6a9e4979a68258
Guri999 <116724657+Guri999@users.noreply.github.com>
2025-07-23 10:06:18 +0900

    Chore: 다크모드 초기화

--- core/designsystem/src/main/java/com/kolown/porring/core/designsystem/ui/theme/Theme.kt
+++ core/designsystem/src/main/java/com/kolown/porring/core/designsystem/ui/theme/Theme.kt
@@ -21,6 +21,7 @@ import androidx.core.view.WindowCompat
 fun PorringTheme(
     content: @Composable () -> Unit,
 ) {
+    DarkThemeController.setDark(isSystemInDarkTheme())
     val isDark = DarkThemeController.isDark
     val colors = remember(isDark) { if (isDark) PorringDarkColor else PorringLightColor }
     val isLightSystemBars = !isDark
